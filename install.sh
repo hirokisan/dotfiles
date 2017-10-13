6bd4ec81ac45a02be2047bfcc516d8c9150431c4
hirokisan <expsharetoworld@gmail.com>
2017-10-13 13:48:53 +0000

    Modify install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ set -eu
 cd ~
 ln -fs dotfiles/vim/.vimrc
 ln -fs dotfiles/.gitconfig
+ln -fs dotfiles/.tigrc
 
 #if [ -f ~/.vim/autoload/plug.vim ];then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
