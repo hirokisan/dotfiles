72894950ec282906fb948f4c9744ecd2cdb66faa
hirokisan <expsharetoworld@gmail.com>
2018-06-27 12:32:10 +0000

    Add .zshrc

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ ln -fs dotfiles/vim/.vimrc.neosnippet
 ln -fs dotfiles/vim/.vimrc.vim-expand-region
 ln -fs dotfiles/.gitconfig
 ln -fs dotfiles/.tigrc
+ln -fs dotfiles/.zshrc
 
 #if [ -f ~/.vim/autoload/plug.vim ];then
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
